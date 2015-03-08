e0bf87488fee93096dce2938838a00c84c4568c1
Flavius12 <info@evolsoft.tk>
2015-03-08 18:33:40 +0100

    Create xWeb.js

---
+++ js/xWeb.js
@@ -0,0 +1,20 @@
+/*
+ * xWeb (v1.0) by EvolSoft
+ * Developer: EvolSoft
+ * Website: http://www.evolsoft.tk
+ * Date: 01/03/2015 06:11 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/xWeb/blob/master/LICENSE)
+ */
+
+//**** Modals ****//
+
+/**
+ * Toggles a modal
+ * 
+ * @param target The target modal
+ */
+function toggleModal(target) {
+    $(target).toggleClass("modal-open");
+}
+
