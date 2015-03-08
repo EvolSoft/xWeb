d4edd6cabd8b9b7bc69dadca97d73f5a670feea8
Flavius12 <info@evolsoft.tk>
2015-03-08 20:17:25 +0100

    Update xWeb.js

--- js/xWeb.js
+++ js/xWeb.js
@@ -2,10 +2,18 @@
  * xWeb (v1.0) by EvolSoft
  * Developer: EvolSoft
  * Website: http://www.evolsoft.tk
- * Date: 01/03/2015 06:11 PM (UTC)
+ * Date: 08/03/2015 07:16 PM (UTC)
  * Copyright & License: (C) 2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/xWeb/blob/master/LICENSE)
  */
+ 
+//**** Alerts ****//
+
+$(document).on("click", ".close", function() {
+	if($(this).parent().hasClass("alert")){
+		$(this).parent().remove();
+	}
+});
 
 //**** Modals ****//
 
@@ -17,4 +25,3 @@
 function toggleModal(target) {
     $(target).toggleClass("modal-open");
 }
-
